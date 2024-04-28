42c85915f6a23bf7736c0bd6f741ee71440d1bbf
Godhani Ripal <ripal.godhani@gmail.com>
2024-04-28 22:11:09 +0530

    allowd host by aws again for user 2

--- plant2/settings.py
+++ plant2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+3-786ff6*c(#p&(bie9l7reb!)4h7b*8*o^8=s=-#gl8&4h5n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.106.194.200']
+ALLOWED_HOSTS = ['3.26.174.198']
 
 
 # Application definition
